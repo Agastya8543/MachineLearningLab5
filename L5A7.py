ec1b8089cfc67e6b66aeef0275316d6101303a14
Agastya8543 <128040809+Agastya8543@users.noreply.github.com>
2023-10-09 10:22:53 +0530

    Add files via upload

---
+++ L5A7.py
@@ -0,0 +1,50 @@
+#7
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+# AND gate data
+X = np.array([[0,0],[0,1],[1,0],[1,1]])
+y = np.array([[0],[0],[0],[1]])
+
+input_size = 2
+hidden_size = 2
+output_size = 1
+
+# Initialize weights and biases
+W1 = np.random.uniform(size=(input_size, hidden_size))
+b1 = np.zeros((1, hidden_size))
+W2 = np.random.uniform(size=(hidden_size, output_size))
+b2 = np.zeros((1, output_size))
+
+learning_rate = 0.05
+
+for iteration in range(10000):
+    # Forward propagation
+    hidden_layer_input = np.dot(X, W1) + b1
+    hidden_layer_output = sigmoid(hidden_layer_input)
+    
+    output_layer_input = np.dot(hidden_layer_output, W2) + b2
+    output = sigmoid(output_layer_input)
+    
+    # Compute error
+    error = y - output
+    if np.mean(np.abs(error)) <= 0.002:
+        break
+
+    # Backpropagation
+    d_output = error * sigmoid_derivative(output)
+    error_hidden_layer = d_output.dot(W2.T)
+    d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
+    
+    # Update weights and biases
+    W2 += hidden_layer_output.T.dot(d_output) * learning_rate
+    b2 += np.sum(d_output, axis=0, keepdims=True) * learning_rate
+    W1 += X.T.dot(d_hidden_layer) * learning_rate
+    b1 += np.sum(d_hidden_layer, axis=0, keepdims=True) * learning_rate
+
+print(output)
